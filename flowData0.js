ab1e0ffdcc6e44433c6e9be8ea5eb9862bcd5689
KishidaSetubiService <atsushi.kishida@kishidasetsubi.com>
2026-01-20 15:27:49 +0900

    Add files via upload

---
+++ flowData0.js
@@ -0,0 +1,12 @@
+console.log("flowData.js loaded");
+const flowData = {
+    start: {
+      type: "dropdown",
+      question: "症状は？",
+      options: {
+        "運転しない": "q0",
+        "冷えが悪い": "q_c0",
+        "暖まりが悪い": "q_h"
+      }
+    }
+};
